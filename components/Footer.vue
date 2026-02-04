8f05f00efaccb2243b2181f8ef32428135e73466
James <146757050+Jameabird@users.noreply.github.com>
2026-02-04 10:05:11 +0700

    update padding footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,9 +4,7 @@ import { Icon } from '@iconify/vue';
 <template>
   <footer class="bg-white">
     <!-- Top section -->
-    <div
-      class="max-w-screen-2xl mx-auto py-12 grid grid-cols-[3fr_2fr_2fr_2fr_2fr] gap-4"
-    >
+    <div class="max-w-screen-2xl mx-auto px-6 lg:px-12 py-12 grid grid-cols-[3fr_2fr_2fr_2fr_2fr] gap-4">
       <!-- Column 1 -->
       <div class="space-y-4">
         <div class="text-3xl font-bold text-blue-600">
