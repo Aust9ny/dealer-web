f84508d3763edf8267a26a98bf47b83d3d97a324
James <146757050+Jameabird@users.noreply.github.com>
2026-02-10 10:51:24 +0700

    Update modal height level

--- components/SearchModal.vue
+++ components/SearchModal.vue
@@ -1,13 +1,10 @@
 <template>
   <div
     v-if="modelValue"
-    class="fixed inset-0 z-50 flex items-center justify-center"
+    class="fixed inset-0 z-50 flex items-start justify-center pt-22"
   >
     <!-- Overlay -->
-    <div
-      class="absolute inset-0 bg-black/40"
-      @click="close"
-    />
+    <div class="absolute inset-0" @click="close" />
 
     <!-- Modal -->
     <div
@@ -91,10 +88,7 @@
             target="_blank"
             class="block mb-3 overflow-hidden rounded-xl border hover:shadow-md"
           >
-            <img
-              :src="banner.image"
-              class="w-full h-36 object-cover"
-            />
+            <img :src="banner.image" class="w-full h-36 object-cover" />
           </a>
           <a
             v-for="banner in banners"
@@ -103,10 +97,7 @@
             target="_blank"
             class="block mb-3 overflow-hidden rounded-xl border hover:shadow-md"
           >
-            <img
-              :src="banner.image"
-              class="w-full h-36 object-cover"
-            />
+            <img :src="banner.image" class="w-full h-36 object-cover" />
           </a>
         </div>
       </div>
@@ -116,18 +107,18 @@
 
 <script setup lang="ts">
 defineProps<{
-  modelValue: boolean
-  trendingProducts: any[]
-  banners: any[]
-  trendingKeywords: string[]
-  searchHistory: string[]
+  modelValue: boolean;
+  trendingProducts: any[];
+  banners: any[];
+  trendingKeywords: string[];
+  searchHistory: string[];
 }>();
 
 const emit = defineEmits<{
-  (e: 'update:modelValue', value: boolean): void
+  (e: "update:modelValue", value: boolean): void;
 }>();
 
 const close = () => {
-  emit('update:modelValue', false);
+  emit("update:modelValue", false);
 };
 </script>
