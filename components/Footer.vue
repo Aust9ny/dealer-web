6245548b1a5e2280c9bc1ab4acb58c87c2d56267
James <146757050+Jameabird@users.noreply.github.com>
2026-02-13 17:57:38 +0700

    Update text "Dealer" in footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -9,7 +9,7 @@ import { Icon } from '@iconify/vue';
       <div class="space-y-4">
         <div class="text-3xl font-bold text-[#0D95DA]">
           Advice <br>
-          <span class="text-green-500">Dealers</span>
+          <span class="text-green-500 italic">Dealers</span>
         </div>
         <div class="mt-2 h-0.5 w-85 bg-gray-200"/>
         <p class="font-bold text-[#0D95DA] text-xl leading-8 tracking-wide">
